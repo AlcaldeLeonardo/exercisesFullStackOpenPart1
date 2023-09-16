baaeeae734d38678c0da4f20c1e82d4b62c1b221
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2023-09-16 16:25:45 -0300

    excersice 1.8 and 1.9 done
    I defined a box Stats component before exercise 1.8

--- exercise-6/src/index.js
+++ exercise-6/src/index.js
@@ -1,64 +1,81 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-const ButtonBox = ({clickGood, clickNeutral, clickBad}) => {
-  return (
-    <div>
-      <h1>Give FeedBack</h1>
-      <Button text="good" handleMouse={clickGood}/>
-      <Button text="neutral" handleMouse={clickNeutral}/>
-      <Button text="bad" handleMouse={clickBad}/>
-    </div>
-  )
-}
+const ButtonBox = ({ clickGood, clickNeutral, clickBad }) => {
+    return (
+        <div>
+            <h1>Give FeedBack</h1>
+            <Button text="good" handleMouse={clickGood} />
+            <Button text="neutral" handleMouse={clickNeutral} />
+            <Button text="bad" handleMouse={clickBad} />
+        </div>
+    );
+};
 
-const StatisticsBox = ({good, neutral, bad}) => {
-  let positive = 0; 
-  let average = 0;
+const StatisticsBox = ({ good, neutral, bad }) => {
+    let positive = 0;
+    let average = 0;
 
-  if(good > bad) average = 1
-  else if(good < bad) average = -1;
-  else average = 0;
+    if (good > bad) average = 1;
+    else if (good < bad) average = -1;
+    else average = 0;
 
-  if(good === 0) {
-    positive = 0;
-  } else {
-    positive = good * 100 / (good + neutral + bad);
-  }
-  return(
-    <div>
-      <h2>Statistics</h2>
-      <p>Good: <span>{good}</span></p>
-      <p>Neutral: <span>{neutral}</span></p>
-      <p>Bad: <span>{bad}</span></p>
-      <p>All: {good + neutral + bad}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive}%</p>
+    if (good === 0) {
+        positive = 0;
+    } else {
+        positive = (good * 100) / (good + neutral + bad);
+    }
+    if (good === 0 && neutral === 0 && bad === 0)
+        return (
+            <div>
+                <h2>Statistics</h2>
+                <p>No feedback given</p>
+            </div>
+        );
+    else
+        return (
+            <div>
+                <h2>Statistics</h2>
+                <p>
+                    Good: <span>{good}</span>
+                </p>
+                <p>
+                    Neutral: <span>{neutral}</span>
+                </p>
+                <p>
+                    Bad: <span>{bad}</span>
+                </p>
+                <p>All: {good + neutral + bad}</p>
+                <p>Average: {average}</p>
+                <p>Positive: {positive}%</p>
+            </div>
+        );
+};
 
-    </div>
-  )
-}
-
-const Button = ({text, handleMouse}) => <button onClick = {handleMouse}>{text}</button>
+const Button = ({ text, handleMouse }) => (
+    <button onClick={handleMouse}>{text}</button>
+);
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
 
-  const increaseGood = () => setGood(good + 1)
-  const increaseNeutral = () => setNeutral(neutral + 1)
-  const increaseBad = () => setBad(bad + 1)
+    const increaseGood = () => setGood(good + 1);
+    const increaseNeutral = () => setNeutral(neutral + 1);
+    const increaseBad = () => setBad(bad + 1);
 
-  return (
-    <div>
-      <ButtonBox clickGood = {increaseGood} clickNeutral={increaseNeutral} clickBad = {increaseBad}/>
-      <StatisticsBox good={good} neutral={neutral} bad={bad}/>
-    </div>
-  )
-}
+    return (
+        <div>
+            <ButtonBox
+                clickGood={increaseGood}
+                clickNeutral={increaseNeutral}
+                clickBad={increaseBad}
+            />
+            <StatisticsBox good={good} neutral={neutral} bad={bad} />
+        </div>
+    );
+};
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById("root"));
